e1a3622b7d802e61a0f8e17c5a97e40d278c489b
shane-221 <shane.mathew98@outlook.com>
2025-03-27 16:05:40 +0000

    Updated files week 12

--- Day 12 -  Scope/Task 2- Number guessing game/Main.py
+++ Day 12 -  Scope/Task 2- Number guessing game/Main.py
@@ -1,50 +1,85 @@
 import random
 
+                    # TODO 1: Prepping the functions and variables
+global Lives
+Lives = 0
+number_loop = True
+game_working = True
+
 
 def difficulty_function():
-    difficulty= input(" Choose a difficulty. Type 'easy' or 'hard':").lower()
+    difficulty = input(" Choose a difficulty. Type 'easy' or 'hard':").lower()
     return difficulty
 
-global Lives
-Lives= 0
-number_loop= True
-
-def number_checker(lives= Lives):
+def number_checker(lives=Lives):
     global number_loop
-    chosen_number= int(input( "Please choose a number:"))
-    if lives==0:
-        number_loop= False
-        print(f"Lives:{lives}")
-    if chosen_number>random_number:
-        print(" You are too high!")
-        lives =lives-1
-        print(f"Lives:{lives}")
-    elif chosen_number<random_number:
-        print("You are too low!")
-        lives = lives - 1
-        print(f"Lives:{lives}")
-    elif chosen_number== random_number:
-        print ("Well done, that's the correct number")
-        number_loop = False
-        print(f"Lives:{lives}")
+    global game_working
 
+    while number_loop:
+        chosen_number = int(input("Please choose a number:"))
+        if lives == 1:
+            print("I'm sorry but you've lost the game!")
+            number_loop = False
+            game_working=False
+            print(f"Lives:{lives}")
+        elif chosen_number > random_number:
+            print(" You are too high!")
+            lives = lives - 1
+            print(f"Lives:{lives}")
+        elif chosen_number < random_number:
+            print("You are too low!")
+            lives = lives - 1
+            print(f"Lives:{lives}")
+        elif chosen_number == random_number:
+            print("Well done, that's the correct number")
+            number_loop = False
+            game_working= False
+            print(f"Lives:{lives}")
 
-                                                                                                                        # TODO 1: Begin the game
-                                                                                                                                #TODO 1a: Opening the game up.
-print("""Welcome to the number guessing game!
-        I'm thinking of a number between 1 and 100. 
-    """)
-random_number = random.randint(1,100)
-print(f"random number: {random_number}")
+        # TODO 2: Begin the game
+                #TODO 1a: Opening the game up
 
-                                                                                                                                # TODO 1b: Choosing difficulty
-game_working= True
-while game_working:
-    difficulty_choice= difficulty_function()
-    if difficulty_choice=="easy":
-        Lives=10
-        while number_loop:
+def game_start(random_number):
+    print("""Welcome to the number guessing game!
+            I'm thinking of a number between 1 and 100. 
+        """)
+    random_number = random.randint(1, 100)
+    print(f"random number: {random_number}")
+
+    # TODO 1b: Choosing difficulty and running it as a loop in case of the else statement.
+
+    while game_working:
+        global Lives
+        difficulty_choice = difficulty_function()
+        if difficulty_choice == "easy":
+            Lives = 10
+            while number_loop:
                 number_checker(lives=Lives)
+
+        elif difficulty_choice == "hard":
+            Lives = 5
+            while number_loop:
+                number_checker(lives=Lives)
+
+        else:
+            print(" Please choose one of the options")
+game_start()
+
+                        # TODO 3: Replay the game once the loops finish.
+keep_running= True
+while keep_running:
+    restart_game = input("Would you like to play the game again? Please choose Y or N")
+    if restart_game == "y":
+        game_start()
+    elif restart_game == "n":
+        print("Thanks for playing my game!")
+        keep_running= False
+    else:
+        print(" That's not one of the options!")
+
+                # This is an infinite/ recursive loop if they keep choosing the wrong one.
+
+
 
 
 
--- Day 12 -  Scope/Task 2- Number guessing game/Xtra files.py
+++
@@ -1,6 +0,0 @@
-elif difficulty == "hard":
-Lives = 5
-
-else:
-print(" Please choose one of the options")
-# need to add it as a loop.
