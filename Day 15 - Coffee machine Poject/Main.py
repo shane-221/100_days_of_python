57334565ab0bf1f10bac1a0bd39bc6ea9df61c65
shane-221 <shane.mathew98@outlook.com>
2025-04-14 15:15:17 +0100

    New files for D15

--- Day 15 - Coffee machine Poject/Main.py
+++ Day 15 - Coffee machine Poject/Main.py
@@ -1,31 +1,20 @@
 from Resources import coffee
 
 
-
-                                                                                                                        # Todo : Ask if they want coffee or other functions
-open_machine= input( "Welcome to the coffee machine! Would you like to order coffee or Run other functions?"
-                     "For coffee - Choose (A). "
-                     "For other functions - Choose (B)\n ").lower()
-while open_machine not in ["a", "b"]:
-    input(" Invalid choice: Please choose 'A' or 'B':")
-
-if open_machine == "a":
-    "run the coffee function"
-else:
-    "print the report function"
-"------------------------------------------------------------------------------------------------------------------------"
                                                                                                                         # Todo Coffee:
                                                                                                                             #   todo:  Ask which coffee they want
 def coffee_function():
-    print(f"""
+    coffee_choice= input(f"""
     {coffee}
-    Welcome to the coffee machine. What can I offer you?
-    For Espresso---> Please choose A. 
-    For latte------> Please choose B. 
-    For Cappuccino-> Please Choose C.  
-    """)
+    Welcome to the coffee machine. What can I offer you? You have a choice between: 
+        1) Espresso
+        2) Latte 
+        3) Cappuccino
+    Please type in the type of coffee you prefer:
+    """).lower()
 
-coffee_function()
+    while coffee_choice not in ["espresso", "latte", "cappuccino"]:
+        coffee_choice= input(" Invalid choice: Please choose 'Espresso' or 'Latte' or 'Cappuccino':")
                                                                                                                             #   todo: Check if there are sufficient resources
                                                                                                                             #   todo: Check payment
                                                                                                                             #   todo: ask if they want to run the coffee function again or get the report or switch off
@@ -33,3 +22,20 @@ coffee_function()
                                                                                                                         # Todo Other functions:
                                                                                                                             #  todo: Print the report function.
                                                                                                                             #  todo: ask if they want to run the coffee function again or get the report or switch off.
+
+"-----------------------------------------------------------------------------------------------------------------------"
+                                                                                                                        # Todo : Ask if they want coffee or other functions
+open_machine= input( """
+                     Welcome to the coffee machine Startup function! "
+                     Would you like to order coffee or Run other functions?"
+                     For coffee - Choose (A). "
+                     For other functions - Choose (B)\n 
+                     """).lower()
+while open_machine not in ["a", "b"]:
+    input(" Invalid choice: Please choose 'A' or 'B':")
+
+if open_machine == "a":
+    coffee_function()
+else:
+    "print the report function"
+"------------------------------------------------------------------------------------------------------------------------"
