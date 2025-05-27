5157bbc3922372b1993012b30bfad984150a6645
shane-221 <shane.mathew98@outlook.com>
2025-05-27 18:42:26 +0100

    New day 16 Files

---
+++ Day 16 - OOP/Main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+# Need to import the class of the turtle class
+
+timmy = Turtle()
+timmy.shape("turtle")
+print(timmy)
+
+
+
+
+# Exit on Click
+my_screen= Screen()
+my_screen.exitonclick()
+
+
+
