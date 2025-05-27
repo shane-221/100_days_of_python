da323dfdeef30bc61fb9814dd17b5ca850b1e66d
shane-221 <shane.mathew98@outlook.com>
2025-05-27 18:48:40 +0100

    Completed walkthrough of Turtle

--- Day 16 - OOP/Walkthrough_of_ Turtle.py
+++ Day 16 - OOP/Walkthrough_of_ Turtle.py
@@ -3,7 +3,13 @@ from turtle import Turtle, Screen
 # Need to import the class of the turtle class
 
 timmy = Turtle()
+    # From arrow to a turtle----attributes 
 timmy.shape("turtle")
+    # From Black colour to red colour---attributes
+timmy.color("red")
+    # Moving the turtle forward---methods:
+timmy.forward(100)
+    # Printing Timmy.
 print(timmy)
 
 
