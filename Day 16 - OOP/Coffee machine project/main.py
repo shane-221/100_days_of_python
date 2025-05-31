0d0f3167344b195beb1f393e29d5c513b42ee29c
shane-221 <shane.mathew98@outlook.com>
2025-05-31 14:48:15 +0100

    Final day files

--- Day 16 - OOP/Coffee machine project/main.py
+++ Day 16 - OOP/Coffee machine project/main.py
@@ -2,5 +2,21 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-order = Menu()
-order.get_item()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu= Menu()
+is_on = True
+
+
+
+while is_on:
+    options =  menu.get_items()
+    choice =  input(f"What would you like to order? [{options}]")
+    if choice =="off":
+        is_on = False
+    elif choice == "report":
+        money_machine.report()
+        coffee_maker.report()
+    else:
+        drink= menu.find_drink(choice)
+        
