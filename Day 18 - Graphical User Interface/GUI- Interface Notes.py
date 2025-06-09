7c2ecbc0bcf9d4aa0fb52366a47b360be10e2dc3
shane-221 <shane.mathew98@outlook.com>
2025-06-09 21:36:32 +0100

    New files for Day 18

---
+++ Day 18 - Graphical User Interface/GUI- Interface Notes.py
@@ -0,0 +1,15 @@
+from turtle import Turtle, Screen
+
+# The instance of Timmy the Turtle being created.
+
+timmy_the_turtle= Turtle()
+    # Changing the shape of the turtle
+timmy_the_turtle.shape("turtle")
+    # Changing the colour of the turtle:
+timmy_the_turtle.color("red")
+
+
+# Exit on click function
+my_screen = Screen()
+my_screen.exitonclick()
+
