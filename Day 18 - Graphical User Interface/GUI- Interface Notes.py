bc95374794299e685d4689571f05b03e74958409
shane-221 <shane.mathew98@outlook.com>
2025-06-09 21:40:42 +0100

    Challenge 1 for day 18

--- Day 18 - Graphical User Interface/GUI- Interface Notes.py
+++ Day 18 - Graphical User Interface/GUI- Interface Notes.py
@@ -1,12 +1,18 @@
 from turtle import Turtle, Screen
 
-# The instance of Timmy the Turtle being created.
+#Todo 1: The instance of Timmy the Turtle being created.
 
 timmy_the_turtle= Turtle()
     # Changing the shape of the turtle
 timmy_the_turtle.shape("turtle")
     # Changing the colour of the turtle:
 timmy_the_turtle.color("red")
+
+# Todo 2: Turtle challenges
+    # Todo 2.A Create a square:
+for i in range(1,5):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 
 # Exit on click function
