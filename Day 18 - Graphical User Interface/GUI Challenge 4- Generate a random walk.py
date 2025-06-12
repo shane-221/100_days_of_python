5946758adaa0cdf4f783746d8756b712aeeb5830
shane-221 <shane.mathew98@outlook.com>
2025-06-12 22:18:58 +0100

    New file adjustment for Challenge 4

--- Day 18 - Graphical User Interface/GUI Challenge 4- Generate a random walk.py
+++ Day 18 - Graphical User Interface/GUI Challenge 4- Generate a random walk.py
@@ -8,7 +8,7 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.pensize(10)
 timmy_the_turtle.speed(7)
 
-# Todo 1: Turtle challenges
+# Todo 1: Turtle functions
 def random_direction():
     direction =[0,90,180, 270]
     final_direction = random.choice(direction)
@@ -22,18 +22,14 @@ def random_colour():
     colour_scheme = (x, y, z)
     return colour_scheme
 
+# Todo 2: Application of functions.
 for i in range(0,100):
     timmy_the_turtle.pencolor(random_colour())
     timmy_the_turtle.forward(50)
     timmy_the_turtle.right(random_direction())
 
 
-
-
-
-
-
-# Exit on click function
+# Todo 3:  Exit on click function
 my_screen = Screen()
 my_screen.setup (width=150, height=150, startx=0, starty=0)
 my_screen.exitonclick()
