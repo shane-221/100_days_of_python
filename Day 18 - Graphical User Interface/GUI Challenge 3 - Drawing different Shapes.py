41cf6050e4ba8922325d93d38ce655ba2948e59f
shane-221 <shane.mathew98@outlook.com>
2025-06-12 22:16:27 +0100

    Challenge 4 for GUI

--- Day 18 - Graphical User Interface/GUI Challenge 3 - Drawing different Shapes.py
+++ Day 18 - Graphical User Interface/GUI Challenge 3 - Drawing different Shapes.py
@@ -10,17 +10,22 @@ turtle.colormode(255)
 # Todo 1: Turtle challenges
 total_angle = 360
 number_of_sides = 3
+
 while number_of_sides<11:
     actual_angle = total_angle / number_of_sides
 
     for i in range(0,number_of_sides):
+                    # Could also place this into a functio and return the random rgb colours 
         x = random.randint(0, 255)
         y = random.randint(0, 255)
         z = random.randint(0, 255)
-        timmy_the_turtle.pencolor(x,y,z)
+        colour_scheme =(x,y,z)
+
+        timmy_the_turtle.pencolor(colour_scheme)
         timmy_the_turtle.forward(100)
         timmy_the_turtle.right(actual_angle)
     number_of_sides+=1
+
 
 # Exit on click function
 my_screen = Screen()
---
+++ Day 18 - Graphical User Interface/GUI Challenge 4- Generate a random walk.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import random
+import turtle
+
+# Colour scheme to be moved from the standard 0 to 1 to o to 255. NEED TO BE FROM THE TURTLE Class itself.
+turtle.colormode(255)
+timmy_the_turtle = Turtle()
+timmy_the_turtle.pensize(10)
+timmy_the_turtle.speed(7)
+
+# Todo 1: Turtle challenges
+def random_direction():
+    direction =[0,90,180, 270]
+    final_direction = random.choice(direction)
+    return final_direction
+
+
+def random_colour():
+    x = random.randint(0, 255)
+    y = random.randint(0, 255)
+    z = random.randint(0, 255)
+    colour_scheme = (x, y, z)
+    return colour_scheme
+
+for i in range(0,100):
+    timmy_the_turtle.pencolor(random_colour())
+    timmy_the_turtle.forward(50)
+    timmy_the_turtle.right(random_direction())
+
+
+
+
+
+
+
+# Exit on click function
+my_screen = Screen()
+my_screen.setup (width=150, height=150, startx=0, starty=0)
+my_screen.exitonclick()
