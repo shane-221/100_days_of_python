8b440816723d8df03058bfe8f13fbd912be83260
shane-221 <shane.mathew98@outlook.com>
2025-06-20 10:46:46 +0100

    Part 1 of pong Game

---
+++ Day 22 - Build Poing ( The Arcade Game)/screen.py
@@ -0,0 +1,12 @@
+import turtle
+from operator import length_hint
+from turtle import Screen
+
+# Todo: Creation of the screen
+screen = Screen()
+screen.setup(width=800,height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+
+screen.exitonclick()
+
