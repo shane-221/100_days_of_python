32e1877bd73b00ca9d44af04eb93d385cd173e91
shane-221 <shane.mathew98@outlook.com>
2025-06-27 23:40:19 +0100

    Scoreboard

--- Day 23- Turtle Crossing Game/scoreboard..py
+++ Day 23- Turtle Crossing Game/scoreboard..py
@@ -1,1 +1,17 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.score = 0
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-260, 260)
+        self.write(self.score,*FONT)
+
