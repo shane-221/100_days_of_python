a909641f076e13ee417d3e207b277684e0218c3b
shane-221 <shane.mathew98@outlook.com>
2025-07-01 15:53:44 +0100

    New files 2

--- Day 24- Mail merge Task/main.py
+++ Day 24- Mail merge Task/main.py
@@ -1,25 +1,24 @@
 
 #TODO: Create a letter using starting_letter.txt
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
 
-
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
+with open("Input/Names/invited_names.txt", mode="r") as file:
+    contents  = file.read()
+    names = contents.splitlines()
 
 #Todo: my Method
-    # Todo: Open the template and pull the thing as a string
-with open("Output/ReadyToSend/example.txt", mode ="w") as template :
-    template.
-
-
     # Todo : open and retrieve the first name
 with open("Input/Names/invited_names.txt", mode ="r") as file:
+            # Need to always read the file to fully access it.
     content  = file.read()
-                # Split lines function splits the text into a list of strings by line.
-    first_name= content.splitlines()[0]
-    print (first_name)
+    for i in names:
+            # Split lines function splits the text into a list of strings by line.
+
+        # Todo: Open the template and pull the thing as a string
+        with open("Input/Letters/example.txt", mode ="r") as template :
+            template_content  = template.read()
+            personal_letter = template_content.replace("[name]", i)
+
+            # Todo: create a new file using the write function
+        with open(f"Output/ReadyToSend/letter_for_{i}", mode= "w") as final_file:
+            final_file.write(personal_letter)
+
