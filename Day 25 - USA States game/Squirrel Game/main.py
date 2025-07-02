67e5718b7a53e5cc33b26ae9f8d0db9db6af8991
shane-221 <shane.mathew98@outlook.com>
2025-07-02 11:30:36 +0100

    new updates

--- Day 25 - USA States game/Squirrel Game/main.py
+++ Day 25 - USA States game/Squirrel Game/main.py
@@ -3,6 +3,28 @@ import pandas as pd
 
 # Todo: Reading the data:
 dataset =  pd.read_csv("Squirrel_Data.csv")
-print (dataset)
 
+    # Todo: breaking up the dataset by colour and column:
+grey = dataset[ dataset["Primary Fur Color" ]== "Gray"]
+cinnamon = dataset[ dataset["Primary Fur Color" ]== "Cinnamon"]
+black = dataset[ dataset["Primary Fur Color" ]== "Black"]
 
+    # Todo: Adding up the columns:
+number_grey= len(grey)
+number_cinnamon= len(cinnamon)
+number_black = len(black)
+
+    # Todo: converting the colours into a dictionary and building them into a dataframe.
+            #  Todo: creating the dictionary to be used.
+Fur_colour_data ={
+    "Gray": number_grey,
+    "Cinnamon": number_cinnamon,
+    "Black": number_black
+                 }
+            # Todo: adjusting the dataset for dataframe
+
+data = pd.Series(Fur_colour_data)
+print( data)
+
+    # Todo: converting the file into a csv file.
+data.to_csv("Fur_Colour_Data.csv")
