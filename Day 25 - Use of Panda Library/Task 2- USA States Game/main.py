ce792e7e5ecb106b3ef26143a2237b09ad3ee2fe
shane-221 <shane.mathew98@outlook.com>
2025-07-04 10:02:19 +0100

    Final files

--- Day 25 - Use of Panda Library/Task 2- USA States Game/main.py
+++ Day 25 - Use of Panda Library/Task 2- USA States Game/main.py
@@ -1,7 +1,6 @@
 import turtle
 import pandas as pd
 FONT = ("Arial",16, "normal")
-import time
 
 # Todo: Creation of the Screen
 screen = turtle.Screen()
@@ -23,30 +22,28 @@ turtle.shape(image)
 
 # Todo: Checking the users answer with all the answers in the csv
 dataset = pd.read_csv("50_states.csv")
-    #  Todo: Convert the states into a list
+    # Todo: Convert the states into a list
 states = dataset.state.to_list()
 
     # Todo: Get the states assumptions running
 score =0
 guessed_states =[]
-            # Todo:  Creating the feedback turtle
+    # Todo:  Creating the feedback turtle
 feedback_t= turtle.Turtle()
-feedback_t
 feedback_t.penup()
 feedback_t.goto(0, -280)
 feedback_t.color("red")
+feedback_t.hideturtle()
 
-# Todo: if the user guesses wrong, then the input box appears again asking for a location.
+# Todo: Running the game as a loop so that they can keep guessing
 game_loop= True
 while game_loop:
-    # Todo: Code to keep making guesses- moving to the state
 
             # Todo:  Applying the question and then requesting the answer:
             # Todo: Title should also keep track of the number of correct answers
     answer_state = screen.textinput(title=f"Guess the state. Score ={score}/ 50",
                                     prompt="What is the states name? Or press N to exit"
                                     )
-            # Todo: Creating the score function:
 
     if answer_state in states:
         t= turtle.Turtle()
@@ -59,16 +56,17 @@ while game_loop:
         t.goto(state_data.x.item(), state_data.y.item())
             # Todo; write the name of the state
         t.write(f"{answer_state}", FONT)
-        states.remove(answer_state)
         guessed_states.append(answer_state)
         score+=1
+        states.remove(answer_state)
     # Todo: What happens if it has already been guessed:
     elif answer_state in guessed_states:
+        feedback_t.clear()
         feedback_t.write("You've already guessed that state!", align="center", font=("Arial", 12, "bold"))
         screen.ontimer(feedback_t.clear, 2000)
 
     # Todo: Codes the exit the loop
-    elif answer_state == "n":
+    elif answer_state == "n" or answer_state=="N":
         game_loop = False
     elif len(states)==0:
         game_loop= False
