1003b65382285c233deedee67947385f8b8dc5ce
shane-221 <shane.mathew98@outlook.com>
2025-07-07 15:03:39 +0100

    Nato game

--- Day 26 - List comprehension/Iterate over Pandas/main.py
+++ Day 26 - List comprehension/Iterate over Pandas/main.py
@@ -7,7 +7,7 @@ student_scores = {"Students":[ "Alice", "Bob", "Charlie" ,"Diana" , "Ethan" ],
 # so far we have looped through a dictionary through the use of (key, value) and .items() to spit the
 ## dictionary into items where we can access the keys separately and the value separately e.g.
 for (key, value) in student_scores.items():
-    print( value)
+    #print( value)
     pass
 
 # Todo 2: Convert the dictionary into a data frame.
@@ -16,12 +16,12 @@ dataset = pd.DataFrame(student_scores)
 print( dataset)
     # Todo: access the data like you would in a dictionary
 for (key, value) in dataset.items():
-    print(value)
+    #print(value)
     pass
 
     # Todo: this is not that useful given that it iterates over the column. Thus panda produces this function :
 for (index, row) in dataset.iterrows():
-    print(  row)
+    #print(  row)
         # This will print everything by rows through each of the index.
         # Todo: you could access a certain specific row through the dot notation
     print( row.Students)
