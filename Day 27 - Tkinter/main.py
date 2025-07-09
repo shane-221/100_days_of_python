cd8c34f283dff098624513b3fbe6487e4b577471
shane-221 <shane.mathew98@outlook.com>
2025-07-09 11:52:30 +0100

    study notes

--- Day 27 - Tkinter/main.py
+++ Day 27 - Tkinter/main.py
@@ -1,17 +1,52 @@
-import tkinter
+from tkinter import *
 
 # Todo: Opening the window. But need to Keep the window open.
-window =tkinter.Tk()
+window =Tk()
 
     # Todo: changing the title of the program:
 window.title("My first GUI Program")
     # Todo: adjust the size of the screen.
 window.minsize(width=500, height = 300)
 
-# Todo: Adding Labels as a class( need to say how it will could be layed out)
-my_label = tkinter.Label(text="I am a Label", font =( " Arial", 24, "bold"))
-    # Todo: Laying out how the lable will be laid out on the screen:
+
+
+# Todo: Creating Labels
+my_label = Label(text="I am a Label", font =( " Arial", 24, "bold"))
+    # Todo: Laying out how the label will be laid out on the screen( Need to do it for all the classes)
 my_label.pack()
+    # Todo: Can change the default properties
+my_label["text"]= "New Text"
+
+
+# Todo: Adding buttons
+    # Todo: How do you get the button to change the title
+def button_click():
+        # When the button gets clicked, the text gets replaced with the specific value
+    # my_label["text"]=" The Button got Clicked"
+        # or you could also:
+    # my_label.config(text="buton got clicked")
+        # When the button gets clicked the text gets replaced with whats on the entry box
+    my_label["text"]=input.get()
+
+# Todo: Entry or Text Boxes
+input  = Entry(width=50)
+input.pack()
+    # Can add whats written in the box to be the title.
+
+
+
+
+
+
+
+
+
+# Todo: Getting the button to exit and work
+button= Button(text = "Click me ", command = button_click)
+button.pack()
+
+
+
 
 # Todo: Keeps the window running
 window.mainloop()
