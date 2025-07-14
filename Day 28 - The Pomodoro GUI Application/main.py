5865b5b3aaba4556c1749d6594f9592ce4311058
shane-221 <shane.mathew98@outlook.com>
2025-07-14 16:36:06 +0100

    Task 1

---
+++ Day 28 - The Pomodoro GUI Application/main.py
@@ -0,0 +1,39 @@
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+# Todo: Building of the screen
+window =Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg =YELLOW)
+
+# Todo: Placing the image on the screen.
+canvas  = Canvas(width =200, height= 224, bg =  YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")          # Image will not learn the tomato file. Hence, need to
+                                                    ## read through the file and get access to the location.
+canvas.create_image(100, 112, image =tomato_img)
+canvas.create_text(100, 130, text ="00:00", fill= "white", font = (FONT_NAME, 35, "bold"))
+canvas.pack()
+                                        # Need to create padding on each side of the picture to make it look cleaner
+
+
+
+
+
+
+window.mainloop()
