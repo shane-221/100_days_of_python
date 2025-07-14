a99a0ce5976700e2cd8f4152cea035998855cfb3
shane-221 <shane.mathew98@outlook.com>
2025-07-14 15:37:31 +0100

    final Files

--- Day 27 - Tkinter/Miles to Kilometer Converter.py
+++ Day 27 - Tkinter/Miles to Kilometer Converter.py
@@ -5,41 +5,47 @@ from tkinter import *
 # Todo: Opening the window. But need to Keep the window open.
 window =Tk()
 
+
     # Todo: changing the title of the program:
 window.title("Miles to Km Converter")
     # Todo: adjust the size of the screen.
 window.minsize(width=300, height = 100)
 
+# Todo: Entry or Text Boxes
+input  =(Entry(width=20))
+input.grid(column=1, row=0)
+
+# Todo: creating the functiont to click
+def calculate_function():
+    miles = float(input.get())
+    final_km= miles*1.609
+    number_Label["text"]=final_km
+
+
 
 
 # Todo: Creating the button to Click:
-button  = Button( text= "Calculate", )
+button  = Button(text= "Calculate", command= calculate_function)
 button.grid(column=1, row=2)
 
 
 
 # Todo: Creating all the text
     # Todo: KM Label
-my_label = Label(text="KM", font =( " Arial", 10, "normal"))
-my_label.grid(column= 2, row =  1)
+km_label = Label(text="KM", font =( " Arial", 10, "normal"))
+km_label.grid(column= 2, row =  1)
 
     # Todo; Creating the changing Km Function
-km =0
-my_label = Label(text=f"{km}", font =( " Arial", 10, "normal"))
-my_label.grid(column= 1, row =  1)
+
+number_Label= Label(text="0", font =( " Arial", 10, "normal"))
+number_Label.grid(column= 1, row =  1)
     # Todo: is equal to part
-my_label = Label(text="is equal to", font =( " Arial", 10, "normal"))
-my_label.grid(column= 0, row =  1)
+equal_to_label = Label(text="is equal to", font =( " Arial", 10, "normal"))
+equal_to_label.grid(column= 0, row =  1)
 
     # Todo: Miles letters
-my_label = Label(text="Miles", font =( " Arial", 10, "normal"))
-my_label.grid(column= 3, row =  0)
-
-
-# Todo: Entry or Text Boxes
-input  = Entry(width=20)
-input.grid(column=1, row=0)
-
+miles_label = Label(text="Miles", font =( " Arial", 10, "normal"))
+miles_label.grid(column= 3, row =  0)
 
 
 
