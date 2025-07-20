1ac70b14f55870d447073cc93033b9c7da40167c
shane-221 <shane.mathew98@outlook.com>
2025-07-20 11:13:54 +0100

    Part 1- Day 30

---
+++ Day 30 - Errors, Exceptions, JSON Data/Study Notes.py
@@ -0,0 +1,21 @@
+#---------------------------------------------File not found error-----------------------------------------------------#
+
+try:                                                # What should have happened. But the file does not exist.
+    file =open("a_file.txt")
+
+except FileNotFoundError:
+                                                    #  Try this if exception. But want it to catch a specific error.
+                                                    #  Can have multiple errors as well.
+    file = open("a_file.txt", "w")
+
+except KeyError as error_message:
+                                                    # Can get a hold pof the error message by the use of as
+    print(f"The key {error_message} does not exist!")
+
+else:
+                                                    # Else Block does not occur - moves onto the exceppt block.
+                                                    # Then second time round it will trigger the else block
+    content  = file.read()
+
+finally:
+    file.close()                                    # Run it no matter what 
