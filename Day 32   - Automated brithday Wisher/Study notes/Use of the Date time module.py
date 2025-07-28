bbfa2f1c7ac9fbd2af948b10ff9b3b68b3c2cde9
shane-221 <shane.mathew98@outlook.com>
2025-07-28 13:54:54 +0100

    day 32-Part 2- Datetime

--- Day 32   - Automated brithday Wisher/Study notes/Use of the Date time module.py
+++ Day 32   - Automated brithday Wisher/Study notes/Use of the Date time module.py
@@ -1,1 +1,15 @@
-import datetime 
+import datetime as dt
+
+now = (dt.datetime.now())
+
+#------------------------------------------------ Today----------------------------------------------------------------#
+year= now.year
+day= now.day
+month= now.month
+weekday  = now.weekday()
+
+
+#------------------------------------------------Choosing another day--------------------------------------------------#
+date_of_birth = dt(year= 1995,
+                   month = 12,
+                   day = 15)
