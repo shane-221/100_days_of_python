1433e3cdf7fe56e054dce6ffd7e21900b0db3842
shane-221 <shane.mathew98@outlook.com>
2025-08-02 12:20:53 +0100

    part 3: Base pieces done

--- Day 34 - Summary of API/Part 3 - Class based Tkinter/main.py
+++ Day 34 - Summary of API/Part 3 - Class based Tkinter/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import UserInterface
 
 question_bank = []
 for question in question_data:
@@ -10,10 +11,18 @@ for question in question_data:
     question_bank.append(new_question)
 
 
-quiz = QuizBrain(question_bank)
 
-while quiz.still_has_questions():
-    quiz.next_question()
+
+
+quiz = QuizBrain(question_bank)
+user_interface= UserInterface()
+user_interface.question_upload(question_text)
+
+
+
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
 
 print("You've completed the quiz")
 print(f"Your final score was: {quiz.score}/{quiz.question_number}")
--- Day 34 - Summary of API/Part 3 - Class based Tkinter/ui.py
+++ Day 34 - Summary of API/Part 3 - Class based Tkinter/ui.py
@@ -1,12 +1,58 @@
+import tkinter
 from tkinter import *
 
 
 #-------------------------------------------------- Constants----------------------------------------------------------#
-
-
-
-
 THEME_COLOR = "#375362"
+FONT_HEADING = ( " Arial", 18, "bold")
+
+
+
+#--------------------------------------------------Class Prep----------------------------------------------------------#
+
+class UserInterface:
+
+
+    def __init__(self):
+        # Todo : creating the window with the background colour
+        self.window= Tk()
+        self.window.config(bg= THEME_COLOR, height=600, width=400, padx=20, pady=20)
+        self.window.title("Trivia Question Game")
+
+        # Todo: creating the canvas
+        self.canvas = Canvas( width= 400, height = 300 , highlightthickness= 0)
+        self.canvas.config( bg= "white" )
+        self.canvas_text = self.canvas.create_text(200, 150, text=f" Some question Text", font=FONT_HEADING)
+        self.canvas.grid( row=1, column=0,  padx = 20, pady= 20, columnspan = 2)
+
+        # Todo : Score Label
+        self.score_label = Label(text="Score:0", fg= "white",bg= THEME_COLOR)
+        self.score_label.grid(row=0, column =1)
+
+
+        # Todo : Adding in the images:
+
+            # Todo Tick or true function.
+        self.true_img =tkinter.PhotoImage(file ="../images/true.png")
+        self.true_button = Button( image= self.true_img, command  = None, width=95, height=95)
+        self.true_button.config(highlightthickness=0, padx=40, pady=40)
+        self.true_button.grid(row=2, column =0)
+            # Todo Cross or False function.
+        self.false_img =tkinter.PhotoImage(file = "../images/false.png")
+        self.false_button = Button( image= self.false_img, command  = None, width=95, height=95)
+        self.false_button.config(highlightthickness=0, padx=40, pady=40)
+        self.false_button.grid(row=2, column = 1,)
+
+        self.window.mainloop()
+
+    # Todo: creating the function that merges the canvas text with the question and answer stuff
+    def question_upload(self, question):
+        self.canvas.itemconfig( self.canvas_text, text =f"Soem question text", font= FONT_HEADING)
+
+
+
+
+
 
 
 
