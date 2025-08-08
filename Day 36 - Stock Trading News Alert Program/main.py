c6fb0558f22002905af04da3c30c7c3ad8530406
shane-221 <shane.mathew98@outlook.com>
2025-08-08 18:25:07 +0100

    Step 1 and 2 complete

--- Day 36 - Stock Trading News Alert Program/main.py
+++ Day 36 - Stock Trading News Alert Program/main.py
@@ -1,54 +1,33 @@
 #------------------------------------------------Imports---------------------------------------------------------------#
 import requests
+import os
 
 
 #---------------------------------------------------Constants ---------------------------------------------------------#
 STOCK = "IBM"
 COMPANY_NAME = "Tesla Inc"
-API_KEY= "JK6ZEULVQTWQL5E4"
+API_KEY= os.environ.get("API_KEY")
 url="http://www.alphavantage.co/query?"
 
-
-#---------------------------------------------------Steps 1 and 2------------------------------------------------------#
-    #Todo: Creating the parameters withe the required arguments
-parameters ={
+#-------------------------------------------------Step 1 AND 2---------------------------------------------------------#
+    #Todo: Creating the parameters withe the required arguments for data
+price_parameters ={
         "apikey": API_KEY,
-        "function":"TIME_SERIES_DAILY",
+        "function":"GLOBAL_QUOTE",
         "symbol": STOCK
 }
-    # Todo: Sending the request
-connection = requests.get(url=url, params= parameters)
-connection.raise_for_status()
-print (connection)
-
-data= connection.json()
-print(data)
+    # Todo: Sending the request FOR the data
+price_request= requests.get(url=url, params= price_parameters)
+data= price_request.json()
 
 
+    # Todo : Pulling the relevant variables out( Current Price, Yesterdays price,and changes
 
-
-
+close_price=int((data["Global Quote"]["08. previous close"]).strip())
+current_price= int((data["Global Quote"]["05. price"]).strip())
+percent_change =data["Global Quote"]["10. change percent"]
 
 
 
 
-
-
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
 
