a5e20c868bc5d17931688a04ec7e463a9f7445f9
shane-221 <shane.mathew98@outlook.com>
2025-08-11 22:48:36 +0100

    Finla files for Day 36 Adjusted

--- Day 35- API Keys, Authentication, Environment Variables/Rain_alert_program/main.py
+++ Day 35- API Keys, Authentication, Environment Variables/Rain_alert_program/main.py
@@ -10,7 +10,7 @@ API_KEY= os.environ.get("API_KEY")
     # Check to see how it works
 print(f"API_KEY loaded: {API_KEY}")
 
-weather_paramas={
+weather_params={
     "lon" : -0.127758,
     "lat" : 51.507351,
     "appid" : API_KEY,
@@ -20,9 +20,12 @@ MY_EMAIL= os.environ.get("MY_EMAIL")
 PASSWORD= os.environ.get("PASSWORD")
 TO_EMAIL=os.environ.get("TO_EMAIL")
 #------------------------------------------------- Request the data----------------------------------------------------#
-response =requests.get(url=OWM_ENDPOINT, params=weather_paramas )
-response.raise_for_status()
-print(response)
+try:
+    response =requests.get(url=OWM_ENDPOINT, params=weather_params)
+    response.raise_for_status()
+except requests.exceptions.HTTPError as e:
+    # This code is from the documentation 
+    print("HTTP error occurred:", e)
 
 #------------------------------------------------ Storing and Using the data-------------------------------------------#
 # Todo: storing the data as a json file
@@ -34,7 +37,7 @@ weather_codes =[int(x["weather"][0]["id"]) for x in weather_data["list"] ]
 
 
 #---------------------------------------------- Sending an email -----------------------------------------------------#
-# Can use the email module to send email if it rains ---> run it in the moning using Python Anywhere and host the code
+# Can use the email module to send email if it rains ---> run it in the morning using Python Anywhere and host the code
 ## on the cloud.
 
 if 700 in weather_codes:
@@ -43,4 +46,8 @@ if 700 in weather_codes:
         connection.login(user= MY_EMAIL, password=PASSWORD)
         connection.sendmail(from_addr=MY_EMAIL,
                             to_addrs=TO_EMAIL,
-                            msg=f"Subject: It will rain!!\n Bring an umbrella please")
+                            msg=f"""
+                            Subject: It will rain!!
+                            
+                            It looks like it will rain today. Please bring an umbrella
+                             """)
