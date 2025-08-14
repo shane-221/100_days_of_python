d0b4f791f813cf7e77a16fb5509ce94b74d643b7
shane-221 <shane.mathew98@outlook.com>
2025-08-14 12:10:50 +0100

    Day 37 : Part 2 and 3

--- Day 38 - Workout Tracking using Google sheets/main.py
+++ Day 38 - Workout Tracking using Google sheets/main.py
@@ -1,18 +1,23 @@
 #---------------------------------------Import modules-----------------------------------------------------------------#
 import requests
+from datetime import datetime
 
 
 #-------------------------------------------Constants that are universal to the code-----------------------------------#
-
+# tODO : Header data for the calorie request
 headers ={
     "x-app-key" :"345f31cc816e55e9990480a85fdc449e",
     "x-app-id": "8e74a439"
 }
-
-url = "https://trackapi.nutritionix.com/v2/natural/exercise"
+# Todo: Calorie constants
+calorie_url = "https://trackapi.nutritionix.com/v2/natural/exercise"
 user_query= input("Tell me which exercises you did?")
 
+# Todo: Sheet Constants
+sheet_url= "https://api.sheety.co/9752de018a3428b69d12669130155572/workoutTracker/sheet1'"
+
 #---------------------------------------------------Parameters---------------------------------------------------------#
+# Todo : Parameters for the calorie API
 exercise_parameters = {
     "weight_kg" : 80,
     "height_cm" : 194,
@@ -21,10 +26,12 @@ exercise_parameters = {
     "query": user_query
                     }
 
+# Todo: Parameter for the Sheetly api to put data into the excel sheet.
+time_now= datetime.now()
 
 
 
 #-------------------------------------------------Requests-------------------------------------------------------------#
-# Todo: Request for the calories
-post_request = requests.post(url=url, headers=headers, json=exercise_parameters )
+# Todo: Request for the exercise parameters to get the calories and time
+post_request = requests.post(url=calorie_url, headers=headers, json=exercise_parameters )
 
