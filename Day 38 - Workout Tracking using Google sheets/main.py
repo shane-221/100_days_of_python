9dea58c3c405b999eea4d1cbfaff6e2cdbc24e45
shane-221 <shane.mathew98@outlook.com>
2025-08-15 23:32:17 +0100

    Day 38: Part 5 complete- adding in authentication

--- Day 38 - Workout Tracking using Google sheets/main.py
+++ Day 38 - Workout Tracking using Google sheets/main.py
@@ -1,20 +1,21 @@
 #---------------------------------------Import modules-----------------------------------------------------------------#
 import requests
 from datetime import datetime
+import os
 
-
-#-------------------------------------------Constants that are universal to the code-----------------------------------#
+#-------------------------------------------Constants that are important to the code-----------------------------------#
 # tODO : Header data for the calorie request
 headers ={
-    "x-app-key" :"345f31cc816e55e9990480a85fdc449e",
-    "x-app-id": "8e74a439"
+    "x-app-key" :os.environ.get("app-key"),
+    "x-app-id": os.environ.get("app-id")
 }
 # Todo: Calorie constants
 calorie_url = "https://trackapi.nutritionix.com/v2/natural/exercise"
 user_query= input("Tell me which exercises you did?")
 
 # Todo: Sheet Constants
-sheet_url= "https://api.sheety.co/9752de018a3428b69d12669130155572/workoutTracker/sheet1"
+sheet_url= os.environ.get("sheet-url")
+sheet_headers = os.environ.get("sheet_headers")
 
 #---------------------------------------------------Calorie request----------------------------------------------------#
 # Todo : Parameters for the calorie API
@@ -30,15 +31,12 @@ exercise_parameters = {
 post_request = requests.post(url=calorie_url, headers=headers, json=exercise_parameters )
     # This is a response object. Then need to convert it into json to be usable
 data =post_request.json()
-print(data)
+print( data)
 #--------------------------------------------sheetly request-----------------------------------------------------------#
 # Todo: Parameter for the Sheetly api to put data into the excel sheet.
 date_time_now= datetime.now()
 date_now=date_time_now.strftime("%x")
 time_now= date_time_now.strftime("%X")
-exercise = data["exercises"][0]["user_input"]
-duration=  data["exercises"][0]["duration_min"]
-calories  = data["exercises"][0]["nf_calories"]
 
 
 
@@ -49,9 +47,9 @@ sheet_params= {"sheet1":
                     "duration":data["exercises"][0]["duration_min"],
                     "calories":data["exercises"][0]["nf_calories"]
                 }}
-print(sheet_params)
+
 # Todo; Send request for the sheetly to google sheets
-sheet_request= requests.post(url = sheet_url, json= sheet_params)
+sheet_request= requests.post(url = sheet_url, json= sheet_params, headers = sheet_headers)
 sheet_request.raise_for_status()
 
-
+#
