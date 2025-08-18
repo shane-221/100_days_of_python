8bf58e1839a9035a91401ea4e07d8670f29cd203
shane-221 <shane.mathew98@outlook.com>
2025-08-18 17:41:21 +0100

    Day 39 Step 2 Complete- Up[dating values through Sheetly

--- Day 39- Flight deal Tracker/main.py
+++ Day 39- Flight deal Tracker/main.py
@@ -3,11 +3,14 @@
 import requests
 import os
 
+
 # ----------------------------------------Pulling the data from Sheetly------------------------------------------------#
 # Todo: Prep part for the initial request
 sheet_url= os.environ.get("sheetly_url")
 sheetly_token =os.environ.get("sheetly_token")
 sheetly_header ={"Authorization": sheetly_token}
+
+
 
 # Todo: Sending a get request
 try:
@@ -19,12 +22,24 @@ finally:
     sheetly_data = data_iata_codes.json()
 
 
+
 #-----------------------------------Getting Countries for  the Amadeus API---------------------------------------------#
 # Todo: Getting a dictionary of interested countries and their prices as key valur pairs for comparison.
-interested_countries ={ x["city"]: x["lowestPrice"] for x in sheetly_data["prices"]}
+countries_iata_code ={ x["city"]: (int(x["id"]),x["iataCode"]) for x in sheetly_data["prices"]}
+
+for i in countries_iata_code:
+    # Todo: First check to see if the IATA Codes are populated. If not you neeed to connect ot Amadeus and find the code
+
+    payload_data={"price":{
+        "iataCode": "Testing"}
+    }
+    if countries_iata_code[i][1]=="":
+        send_iata_code_request = requests.put(url=  f"{sheet_url}/{countries_iata_code[i][0]}",
+                                              headers=sheetly_header,
+                                              json = payload_data)
+        print( send_iata_code_request.status_code)
 
 
 
 
-# ------------------------------------------Place the codes into the Sheetly API---------------------------------------#
 
