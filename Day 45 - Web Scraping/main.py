56b22c46f0ccc9860eedf3ac457c10de5ccbd078
shane-221 <shane.mathew98@outlook.com>
2025-09-08 22:47:17 +0100

    Day 45 Task 1 Complete

--- Day 45 - Web Scraping/main.py
+++ Day 45 - Web Scraping/main.py
@@ -1,3 +1,33 @@
+# Todo: Importing beautiful soup verison 4
+from bs4 import BeautifulSoup
+
+
+
 # Todo:  Opening and collecting the data from the html file;
 with open("./website.html") as file:
-     content  = file.read()
+     contents = file.read()
+
+
+# Todo: Using the beautuful soup module
+                # notes class( MARKUP(I.E. the code for the html), parser( what language?))
+soup = BeautifulSoup(contents,"html.parser" )
+                        # In some cases you may need to use lmxl parser. Parser is just a way to read the website
+                        # The soup acts as an object hat can tap into any part of the html code. Therefore allowing you
+                        ## to tap into the html object using python code.
+
+#---------------------------------------Functionalities----------------------------------------------------------------#
+
+# Entire tag
+print(soup.title)
+
+# Tag function
+print(soup.title.name)
+
+# Tag string
+print(soup.title.string)
+
+# Can use prettify to place the indentations
+print(soup.prettify())
+
+# Can also get the first anchor tag/ li/p:
+print(soup.li)
