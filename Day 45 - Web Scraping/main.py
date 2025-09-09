4f07b1139df9aca29da5dc0429592a4ccedff5ea
shane-221 <shane.mathew98@outlook.com>
2025-09-09 23:37:56 +0100

    Day 45 Web scraping notes

--- Day 45 - Web Scraping/main.py
+++ Day 45 - Web Scraping/main.py
@@ -12,11 +12,11 @@ with open("./website.html") as file:
                 # notes class( MARKUP(I.E. the code for the html), parser( what language?))
 soup = BeautifulSoup(contents,"html.parser" )
                         # In some cases you may need to use lmxl parser. Parser is just a way to read the website
-                        # The soup acts as an object hat can tap into any part of the html code. Therefore allowing you
+                        # The soup acts as an object hat can tap into any part of the html code. Therefore, allowing you
                         ## to tap into the html object using python code.
 
 #---------------------------------------Functionalities----------------------------------------------------------------#
-
+            # All of these functionalities that only capture the first instance of it
 # Entire tag
 print(soup.title)
 
@@ -31,3 +31,21 @@ print(soup.prettify())
 
 # Can also get the first anchor tag/ li/p:
 print(soup.li)
+
+# Can use the function ---->find_all( name = tag name)
+all_anchor_tags = soup.find_all(name = "a")
+
+    # Can also capture the text within the anchor tag using:
+for tag in all_anchor_tags:
+    print( tag.getText)
+    pass
+
+for tag in all_anchor_tags:
+    # Need to isolate the link within the tag
+    tag.get("href")
+
+# Can also search using the identifiers to find the tag of the identifier( class and tags)
+heading = soup.find(name= "h1" , id="name")
+            #soup.find is only applicable for a first instance of an event
+            #same thing applied to clas function. Class is a reserve function. Onely can be used to create classes. Therefore
+            ## need an underscore when you cal it 
