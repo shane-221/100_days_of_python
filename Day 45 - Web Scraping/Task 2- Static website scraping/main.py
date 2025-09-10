fd192c1d3923aad69d861c4df04838bd74da3755
shane-221 <shane.mathew98@outlook.com>
2025-09-10 22:38:18 +0100

    Day 45 2/3 done for the live website

--- Day 45 - Web Scraping/Task 2- Static website scraping/main.py
+++ Day 45 - Web Scraping/Task 2- Static website scraping/main.py
@@ -22,13 +22,20 @@ article_link =headings.get("href")
 #Todo: Finding all of the headings, link and the upvotes
     # Same thing but just the find all function
 headings= soup.find_all(name ="a",class_ ="storylink")
+article_list_text=[]
+articles_list_links=[]
 
 for article_tag in headings:
-    
-upvote_function =soup.find_all(name="span", class_="score")
-upvotes = upvote_function.getText()
+
+    article_text = article_tag.getText()
+    article_list_text.append(article_text)
+
+    article_link = article_tag.get("href")
+    articles_list_links.append(article_link)
+
+article_upvotes = [score.getText()for score in soup.find_all(name="span", class_= "score")]
+        # All the lists are now ordered. Need to get the article numbers to be in number format. Remove Points.
+article_upvotes = [int(i.split()[0]) for i in article_upvotes]
+print(article_upvotes)
 
 
-article_upvotes = upvotes
-article_text = headings.getText()
-article_link =headings.get("href")
