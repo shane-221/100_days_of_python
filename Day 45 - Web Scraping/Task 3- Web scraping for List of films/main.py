f3f2f7bc4b7aa8c13945796ae87c53eae4e4eb51
shane-221 <shane.mathew98@outlook.com>
2025-09-11 19:17:11 +0100

    List of movies reached, need to strip the string marks

--- Day 45 - Web Scraping/Task 3- Web scraping for List of films/main.py
+++ Day 45 - Web Scraping/Task 3- Web scraping for List of films/main.py
@@ -6,7 +6,20 @@ from bs4 import BeautifulSoup
 #Todo: ------------------------------------Getting the list of movies in TXT Format------------------------------------#
 connection = requests.get(url="https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
 website = connection.text
-print(website)
 
 
-#Todo: ----------------------------------------Using beaultiful soup to get the data required--------------------------#
+#Todo: ----------------------------------------Using beautiful soup to get the data required--------------------------#
+movie_titles=[]
+soup = BeautifulSoup(website, "html.parser")
+heading =soup.find_all(name="h3" , class_="title")
+
+
+for tag in heading:
+    movie_name = tag.getText().split()
+    movie_name = movie_name[1:]
+    result =""
+    for i in movie_name:
+        result += i + " "
+    movie_titles.append(result.strip(''))
+
+movie_titles =
