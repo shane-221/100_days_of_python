598ff2604d52383ef52a0cfc92e9207aa96eae1c
shane-221 <shane.mathew98@outlook.com>
2025-09-23 13:03:10 +0100

    Part 2 50%

---
+++ Day 48- Selenium WebDriver/main2.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Setting the page so it stays open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("Detach", True)
+
+
+# Getting the webdriver
+driver= webdriver.Chrome(options=chrome_options)
+driver.get("https://appbrewery.github.io/instant_pot/")
+
+# Finding stuff by classes
+price_dollar= driver.find_element(By.CLASS_NAME, "a-price-whole").text
+price_pennies= driver.find_element(By.CLASS_NAME, "a-price-fraction").text
+
+print(f"{price_dollar}.{price_pennies}")
+
+
+# Finding stuff by the name
+search_bar = driver.find_element(By.NAME,"q")
+print(search_bar.get_attribute("placeholder"))
+
+driver.quit()
