87e34794baffa20e689a563b57d72cbcb5df1505
shane-221 <shane.mathew98@outlook.com>
2025-09-23 12:26:34 +0100

    New files prep

---
+++ Day 48- Selenium WebDriver/main.py
@@ -0,0 +1,10 @@
+from  selenium import webdriver
+
+# Using the webdriver
+driver= webdriver.Chrome()
+
+# Configuring the webdriver
+chrome_options= webdriver.ChromeOptions()
+
+
+driver.get("http://www.amazon.com")
