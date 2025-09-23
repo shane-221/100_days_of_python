af7b0154c27eacc2547eee47068d08f7d1e796b6
shane-221 <shane.mathew98@outlook.com>
2025-09-23 12:32:45 +0100

    pART 1

--- Day 48- Selenium WebDriver/main.py
+++ Day 48- Selenium WebDriver/main.py
@@ -1,10 +1,16 @@
 from  selenium import webdriver
 
-# Using the webdriver
-driver= webdriver.Chrome()
+
 
 # Configuring the webdriver
 chrome_options= webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
 
-
+# Using the webdriver
+driver= webdriver.Chrome(options =  chrome_options)
 driver.get("http://www.amazon.com")
+
+
+# Closing or quitting:
+driver.quit()
+driver.close()
