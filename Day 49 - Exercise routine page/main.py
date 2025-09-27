a8190a7ff3fcb5b32b17cf23134a94022cd825fc
shane-221 <shane.mathew98@outlook.com>
2025-09-27 23:25:16 +0100

    Day 49- Login complete

--- Day 49 - Exercise routine page/main.py
+++ Day 49 - Exercise routine page/main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from dotenv import load_dotenv
+load_dotenv()
 
 #Todo:---------------------------------------------- Prep--------------------------------------------------------------#
 
@@ -23,5 +25,19 @@ driver = webdriver.Chrome( options= chrome_options)
 driver.get("https://appbrewery.github.io/gym/")
 
 # Todo--------------------------------------- Logging into the website-------------------------------------------------#
+
+            # Reaching the login page
 login_button =  driver.find_element(By.CLASS_NAME, value= "Navigation_rightSection__L2XbA")
 login_button.click()
+
+            # Logging into the page
+email= driver.find_element(By.CSS_SELECTOR, value = "input[id='email-input']")
+email_id =os.getenv("USER_EMAIL")
+email.send_keys(email_id)
+
+password= driver.find_element(By.CSS_SELECTOR, value = "input[id='password-input']")
+password_id =os.getenv("USER_PASSWORD")
+password.send_keys(password_id)
+
+login= driver.find_element(By.ID, value="submit-button")
+login.click()
