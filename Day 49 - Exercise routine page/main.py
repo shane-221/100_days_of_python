a45d7149c49f7f354c33c1c1eeee336fda729dca
shane-221 <shane.mathew98@outlook.com>
2025-09-27 23:00:52 +0100

    Day 49- Got to the login page

--- Day 49 - Exercise routine page/main.py
+++ Day 49 - Exercise routine page/main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 #Todo:---------------------------------------------- Prep--------------------------------------------------------------#
 
@@ -18,3 +21,7 @@ chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome( options= chrome_options)
 driver.get("https://appbrewery.github.io/gym/")
+
+# Todo--------------------------------------- Logging into the website-------------------------------------------------#
+login_button =  driver.find_element(By.CLASS_NAME, value= "Navigation_rightSection__L2XbA")
+login_button.click()
