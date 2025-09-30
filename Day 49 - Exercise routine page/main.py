902711ba7e1c7a23d2ae095cbea689be2d63daeb
shane-221 <shane.mathew98@outlook.com>
2025-09-30 18:19:51 +0100

    Day 49 Part 5 nearly complete

--- Day 49 - Exercise routine page/main.py
+++ Day 49 - Exercise routine page/main.py
@@ -9,13 +9,13 @@ from selenium.webdriver.support import expected_conditions as ec
 
 #Todo:---------------------------------------------- Prep--------------------------------------------------------------#
 
-    #Tells the webpage to stay open
+        #Tells the webpage to stay open
 
 chrome_options= webdriver.ChromeOptions()
 chrome_options.add_experimental_option(name ="detach", value = True)
 
-    #Specifying a custom location to store the data etc. Combines the current file with the one stated into a full
-    #path
+        #Specifying a custom location to store the data etc. Combines the current file with the one stated into a full
+        #path
 user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
             #Specifies the correct file to be used.
 chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
@@ -52,6 +52,29 @@ wait.until(ec.presence_of_element_located((By.ID, "schedule-page")))
 # Todo---------------------------------------Booking classes-----------------------------------------------------------#
 
 
-        # Choosing The day
-date = driver.find_element(By.ID, value= "day-group-tue,-sep-30")
-date.text
+            # Choosing The day
+date = driver.find_element(By.ID, value= "day-group-mon,-oct-6")
+activities = date.find_elements(By.CLASS_NAME, value="ClassCard_card__KpCx5")
+
+            # Adding the events of the daya into a dict
+for i in activities:
+                # Check to see if any classes are not fully booked and printing the message
+    have_booked = i.get_attribute("data-user-booked")
+    fully_booked = i.get_attribute("data-available-spots")
+    waitlisted = i.get_attribute("data-user-waitlisted")
+
+    event_data = i.get_attribute("id").split("-")                           # Getting the event and time data to present
+    event = event_data[2]
+    time =event_data[-1]
+
+    if str(have_booked)=="true":                                                 # Check 1: have they booked it
+        print(f"✓ Have booked : {event} on Mon, Oct 6 at {time}")
+        print("Script started")
+    elif str(fully_booked)=="true":                                              # Check 2: Can they book it ?
+        print(f"✓ Event full : {event} on Mon, Oct 6 at {time}")
+    elif str(fully_booked)=="false":                                             # Check 3: Are they on the waitlist
+        if str(waitlisted)=="true":
+            print(f"✓ Already on waitlist: {event} on Mon, Oct 6 at {time}")
+        else:                                                               # Check 4 If not on the Waitlist: Ask to check
+            print(f"{event} at {time}")
+            response = input("Would you like to be waitlisted for this event?")
