43633edd96117fc1b0a77544d0d239d35fe78e42
shane-221 <shane.mathew98@outlook.com>
2025-10-03 11:22:10 +0100

    Day 49: Step 6: Multiple dates and final sumamry improved

--- Day 49 - Exercise routine page/main.py
+++ Day 49 - Exercise routine page/main.py
@@ -6,23 +6,25 @@ load_dotenv()
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
-DATE = "day-group-mon,-oct-6"
+DATES = ["day-group-mon,-oct-6"]
 Classes_booked = 0
 Waitlists_joined = 0
 Total_classes_processed =  0
+Booked_classes_details =[]
+Waiting_classes_details=[]
 
 
 #Todo:---------------------------------------------- Prep--------------------------------------------------------------#
 
-        #Tells the webpage to stay open
+                                                                                                                        #Tells the webpage to stay open
 
 chrome_options= webdriver.ChromeOptions()
 chrome_options.add_experimental_option(name ="detach", value = True)
 
-        #Specifying a custom location to store the data etc. Combines the current file with the one stated into a full
-        #path
+                                                                                                                        #Specifying a custom location to store the data etc. Combines the current file with the one stated into a full
+                                                                                                                        #path
 user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
-            #Specifies the correct file to be used.
+                                                                                                                        #Specifies the correct file to be used.
 chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 
@@ -34,11 +36,11 @@ driver.get("https://appbrewery.github.io/gym/")
 
 # Todo--------------------------------------- Logging into the website-------------------------------------------------#
 
-            # Reaching the login page
+                                                                                                                        # Reaching the login page
 login_button =  driver.find_element(By.CLASS_NAME, value= "Navigation_rightSection__L2XbA")
 login_button.click()
 
-            # Logging into the page
+                                                                                                                        # Logging into the page
 email= driver.find_element(By.CSS_SELECTOR, value = "input[id='email-input']")
 email_id =os.getenv("USER_EMAIL")
 email.send_keys(email_id)
@@ -50,7 +52,7 @@ password.send_keys(password_id)
 login= driver.find_element(By.ID, value="submit-button")
 login.click()
 
-            # Wait for schedule page to load
+                                                                                                                        # Wait for schedule page to load
 wait.until(ec.presence_of_element_located((By.ID, "schedule-page")))
 
 
@@ -58,63 +60,78 @@ wait.until(ec.presence_of_element_located((By.ID, "schedule-page")))
 
 # Todo---------------------------------------Booking classes-----------------------------------------------------------#
 
+    # Todo:------------------------------------Condition for multiple dates-----------------------------#
+                                                                                                                        # Checking the condition for multiple dates as a list
+for dates in DATES:
+
+            # Todo:-------------- Getting all the important data prepared ------------------#
+                                                                                                                        # Choosing The day
+    date = driver.find_element(By.ID, value= dates)
+    activities = date.find_elements(By.CLASS_NAME, value="ClassCard_card__KpCx5")
 
 
-        # Todo:-------------- Getting all the important data prepared ------------------#
-            # Choosing The day
-date = driver.find_element(By.ID, value= DATE)
-activities = date.find_elements(By.CLASS_NAME, value="ClassCard_card__KpCx5")
+                                                                                                                        # Adding the events of the daya into a dict
+    for i in activities:
+        Total_classes_processed+=1
+                                                                                                                        # Check to see if any classes are not fully booked and printing the message
+        have_booked = str(i.get_attribute("data-user-booked")).strip().lower()
+        fully_booked = str(i.get_attribute("data-class-status")).strip().lower()
+        waitlisted = str(i.get_attribute("data-user-waitlisted")).strip().lower()
+
+        event_data = i.get_attribute("id").split("-")                                                                   # Getting the event and time data to present
+        event = str(event_data[2]).strip().lower()
+        time =str(event_data[-1]).strip().lower()
+
+                # Todo:---------------------Click Functions ---------------------------------#
+        try:
+            all_clicks = i.find_element(By.CLASS_NAME, value="ClassCard_cardActions__tVZBm")
+            button = all_clicks.find_element(By.CSS_SELECTOR, value="button")
+            driver.execute_script("arguments[0].scrollIntoView(true);", button)
+        except Exception as e:
+            print (f" Could not find the Button for {event} at {time}: Error {e}")
 
 
-            # Adding the events of the daya into a dict
-for i in activities:
-    Total_classes_processed+=1
-                # Check to see if any classes are not fully booked and printing the message
-    have_booked = str(i.get_attribute("data-user-booked")).strip().lower()
-    fully_booked = str(i.get_attribute("data-class-status")).strip().lower()
-    waitlisted = str(i.get_attribute("data-user-waitlisted")).strip().lower()
+                #Todo:---------Checking througgh the logic to click ther buttons--------------#
+        if have_booked=="true":                                                                                         # Check 1: have they booked it
+            print(f"✓ Already Booked: {event} on Mon, Oct 6 at {time}")
+            Classes_booked+=1
+            Booked_classes_details.append(f"[Already booked]:{event} on Mon, Oct 6 at {time}")
 
-    event_data = i.get_attribute("id").split("-")                         # Getting the event and time data to present
-    event = str(event_data[2]).strip().lower()
-    time =str(event_data[-1]).strip().lower()
+        elif waitlisted=="true":
+            print(f"✓ Already Waitlisted : {event} on Mon, Oct 6 at {time}")
+            Waitlists_joined+= 1
+            Waiting_classes_details.append(f"[Already Waitlisted]:{event} on Mon, Oct 6 at {time}")
 
-            # Todo:---------------------Click Functions --------------------------------------#
-    try:
-        all_clicks = i.find_element(By.CLASS_NAME, value="ClassCard_cardActions__tVZBm")
-        button = all_clicks.find_element(By.CSS_SELECTOR, value="button")
-        driver.execute_script("arguments[0].scrollIntoView(true);", button)
-    except Exception as e:
-        print (f" Could not find the Button for {event} at {time}: Error {e}")
-
-
-            #Todo:----------------------Checking througgh the logic to click ther buttons--------------#
-    if have_booked=="true":                                               # Check 1: have they booked it
-        print(f"✓ Have booked : {event} on Mon, Oct 6 at {time}")
-        Classes_booked+=1
-
-    else:
-         if fully_booked=="full":                                         # Check 2: Can they book it ?
-            print(f"✓ Event full : {event} on Mon, Oct 6 at {time}")
-            response1 = input("Would you like to be waitlisted for this event? Yes or No?").lower().strip()
-            if response1=="yes":
-                button.click()
-                Waitlists_joined+=1
-            else:
-                pass
-                                                                   # Check 3: Are they on the waitlist
-         elif fully_booked == "available":
-                print(f"✓ Event Available : {event} on Mon, Oct 6 at {time}")
-                response2 = input("Would you like to book this event? Yes or No?").lower().strip()
-                if response2=="yes":
+        else:
+             if fully_booked=="full":                                                                                   # Check 2: Can they book it ?
+                print(f"✓ Event full : {event} on Mon, Oct 6 at {time}")
+                response1 = input("Would you like to be waitlisted for this event? Yes or No?").lower().strip()
+                if response1=="yes":
                     button.click()
-                    Classes_booked+=1
+                    Waitlists_joined+=1
+                    Waiting_classes_details.append(f"[New Waitlist]:{event} on Mon, Oct 6 at {time}")
                 else:
                     pass
+                                                                                                                        # Check 3: Are they on the waitlist
+             elif fully_booked == "available":
+                    print(f"✓ Event Available : {event} on Mon, Oct 6 at {time}")
+                    response2 = input("Would you like to book this event? Yes or No?").lower().strip()
+                    if response2=="yes":
+                        button.click()
+                        Classes_booked+=1
+                        Booked_classes_details.append(f"[New booking]: {event} on Mon, Oct 6 at {time}")
+                    else:
+                        pass
 
-
+# Todo---------------------------------------Printing the final summary------------------------------------------------#
 print(f"""
-      --- BOOKING SUMMARY ---
-        Classes booked: {Classes_booked}
-        Waitlists joined: {Waitlists_joined}
-        Total classes processed: {Total_classes_processed}
-      """)
+--- BOOKING SUMMARY ---
+Classes booked: {Classes_booked}
+Waitlists joined: {Waitlists_joined}
+Total classes processed: {Total_classes_processed}\n
+""")
+print("--- DETAILED CLASS LIST ---")
+for i in Booked_classes_details:
+    print(i)
+for i in Waiting_classes_details:
+    print(i)
