6e31ede4c5bb2c5c4faf616887de5eebeff6ef51
shane-221 <shane.mathew98@outlook.com>
2025-10-09 15:58:29 +0100

    Day 54@: Part 4 Complete

--- Day 54 - Intermediate - Web Development/Hello 2 -  From script.py
+++ Day 54 - Intermediate - Web Development/Hello 2 -  From script.py
@@ -10,3 +10,6 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
+if __name__ == "__main__":
+    app.run()
+    # Name is the same as __main__ therefore this code makes it run as a script
