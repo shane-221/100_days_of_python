324fe81cda59742c585ccffa57cdec7a1bd9523c
shane-221 <shane.mathew98@outlook.com>
2025-10-13 17:08:12 +0100

    D55 - Task 1

--- 55_Day55 -URL Parsing and HTML/Task 1.py
+++ 55_Day55 -URL Parsing and HTML/Task 1.py
@@ -5,6 +5,21 @@ app = Flask("website")
 
 @app.route("/")
 def home():
-    return print("Welcome to my homepage")
+    return "Welcome to my homepage"
+
+@app.route("/username/<name>")
+def greet(name):
+    return f"Hello {name}. Welcome to my homepage"
+
+
+@app.route("/username/<names>/<number>")
+def greeting(names, number):
+    return f"""
+                Hello {names}. Welcome to my homepage
+                Hello {number}. Welcome to my homepage
+            """
+
+if __name__=="__main__":
+    app.run(debug= True)
 
 
