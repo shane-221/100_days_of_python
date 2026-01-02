6baef38a126e92b5962da1beae5f98a0c0a5704e
shane-221 <shane.mathew98@outlook.com>
2026-01-02 18:00:47 +0000

    Day 36 progress commit

--- 36_Day 36 - Stock Trading News Alert Program/Structures/company_website_section.py
+++ 36_Day 36 - Stock Trading News Alert Program/Structures/company_website_section.py
@@ -6,13 +6,19 @@ class CompanyWebsiteSection:
         self.article_number=-1
 
     def structure (self):
+        articles_html = ""
+        for a in self.articles:
+            articles_html += f"<li>{a[0]} -- {a[1]} -- {a[2]}\n</li>"
+
+        print(articles_html)
+
         return f"""
-        <div>
-            <h3>Company Stock ID: {self.stock}</h3>
-            <h3>Company Price Change: {self.price_change}</h3>
-            <h3>Articles</h3>
-            <ul style="margin-left: 20px;">
-                <li>{self.articles[self.article_number+1][0]} I {self.articles[self.article_number+1][1]} I {self.articles[self.article_number+1][2]}/li>
-        <div>
-    
+            <div>
+                <h3>Company Stock ID: {self.stock}</h3>
+                <h3>Company Price Change: {self.price_change}</h3>
+                <h3>Articles</h3>
+                <ul style="margin-left: 20px;" >
+                    {articles_html}
+                </ul>
+            </div>
         """
